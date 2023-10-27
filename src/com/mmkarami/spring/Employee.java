81e114b6ee446d1dbff21e188b0b9a75b76e2173
MohamadMehdiK <MohamadMehdiK@SystemGroup.Net>
2023-10-27 23:33:53 +0330

    Autowired annotation is added.

--- src/com/mmkarami/spring/Employee.java
+++ src/com/mmkarami/spring/Employee.java
@@ -1,15 +1,13 @@
 package com.mmkarami.spring;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Employee implements Person {
 
+	@Autowired
 	Printer<Bill> printer;
-	
-	public Employee(Printer<Bill> printer) {
-		this.printer = printer;
-	}
 
 	@Override
 	public void printBill(Bill bill) {
